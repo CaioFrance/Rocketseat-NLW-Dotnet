e8c5202812c579e05fee0a6e06b0bb86b0dc2320
Caio Francelino <caio.francelinosena@gmail.com>
2024-02-07 01:01:46 -0400

    NLW dia 1

---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var rootDirectory = Environment.CurrentDirectory;
+        var dataDirectory = Path.GetFullPath(Path.Combine(rootDirectory, "Data"));
+        var dbFilePath = Path.Combine(dataDirectory, "leilaoDbNLW.db");
+
+        optionsBuilder.UseSqlite($"Data Source={dbFilePath}");
+    }
+}
